4727211b1a88e0da4b592b35230cf90922f5939d
Sam-Arul <61419602+Sam-Arul@users.noreply.github.com>
2020-05-06 15:38:35 +0530

    heroku connect

--- src/app/url.service.ts
+++ src/app/url.service.ts
@@ -10,12 +10,12 @@ export class UrlService {
   constructor(private http:HttpClient) { }
 
   postUrl(data):Observable<any>{
-    return this.http.post("http://localhost:3000",data)
+    return this.http.post("https://sam-url-shortener.herokuapp.com/",data)
   }
   geturl():Observable<any>{
-    return this.http.get("http://localhost:3000/url")
+    return this.http.get("https://sam-url-shortener.herokuapp.com/url")
   }
   redirecturl(data):Observable<any>{
-    return this.http.get("http://localhost:3000/url/"+data)
+    return this.http.get("https://sam-url-shortener.herokuapp.com/url/"+data)
   }
 }
